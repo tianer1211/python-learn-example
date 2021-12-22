bd152f9a5b6a4d5214d2fcabf340be9a639dfe31
tianer1211 <tianer1211@gmail.com>
2021-12-22 14:03:32 +0800

    Update openpyxl_learn.py

--- openpyxl_learn.py
+++ openpyxl_learn.py
@@ -4,7 +4,10 @@ from openpyxl import Workbook
 wb = Workbook() # 创建一个Excel，默认会创建一个'Sheet'
 
 # 以下二选一
+#1.
 work_sheet = wb.active  # 获取初始的默认sheet
+work_sheet.title = 'xxx' # 修改当前默认title 名称
+#2.
 work_sheet = wb.create_sheet(index=0, title='XXXX')
 
 # 写入数据
